f907241def744250090bca73384fb5c005816c0e
Barun Thapa <barunthapa.bvdt@gmail.com>
2012-12-04 20:13:37 +0545

    create a repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExploreNepal::Application.config.secret_token = '0c52375ef79e0aef4bd39988666b8041a98960c50bc8c78a55e293ec366efd6038b57db5a449caa6eed0f93e3fb01087f1c7f15a32be2efc99339c76499f8526'
